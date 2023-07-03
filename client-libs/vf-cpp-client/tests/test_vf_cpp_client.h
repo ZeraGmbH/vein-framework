19083cc6d46ffa61e12839c24840aecc507b8160
Andreas Müller <schnitzeltony@gmail.com>
2023-07-03 15:49:19 +0200

    vf-cpp-client: Introduce tests - no useful implementation yet
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ client-libs/vf-cpp-client/tests/test_vf_cpp_client.h
@@ -0,0 +1,14 @@
+#ifndef TEST_VF_CPP_CLIENT_H
+#define TEST_VF_CPP_CLIENT_H
+
+#include <QObject>
+
+class test_vf_cpp_client : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void introspectionTest();
+};
+
+#endif // TEST_VF_CPP_CLIENT_H
