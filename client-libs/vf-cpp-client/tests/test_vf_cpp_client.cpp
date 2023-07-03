2aee1a1f99458ba1cc619fba66fd30d8c16bba14
Andreas Müller <schnitzeltony@gmail.com>
2023-07-03 18:28:25 +0200

    ModmanBase: Add a test for empty modman / reset vf-cpp-client test
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- client-libs/vf-cpp-client/tests/test_vf_cpp_client.cpp
+++ client-libs/vf-cpp-client/tests/test_vf_cpp_client.cpp
@@ -1,6 +1,12 @@
 #include "test_vf_cpp_client.h"
+#include "modulemanagercontroller.h"
 #include "ve_eventhandler.h"
 #include "vf-cpp-entity.h"
+#include "vftestcomponentaddlistener.h"
+#include "vftestcomponentchangelistener.h"
+#include "vn_introspectionsystem.h"
+#include "vs_veinhash.h"
+#include <QAbstractEventDispatcher>
 #include <QTest>
 
 QTEST_MAIN(test_vf_cpp_client)
@@ -11,6 +17,9 @@ void test_vf_cpp_client::init()
 
 void test_vf_cpp_client::introspectionTest()
 {
-    VeinEvent::EventHandler vfEventHandler;
-    VfCpp::veinmoduleentity systemEntity(0);
+}
+
+void test_vf_cpp_client::feedEventLoop()
+{
+    while(QCoreApplication::eventDispatcher()->processEvents(QEventLoop::AllEvents));
 }
