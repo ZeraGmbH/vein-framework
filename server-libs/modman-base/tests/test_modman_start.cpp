5dd23c7a9178f366c1e0e5624c2f69fc066a019c
Andreas Müller <schnitzeltony@gmail.com>
2023-07-04 21:06:28 +0200

    Work on test_modman_start::emptyModman() more to understand code better
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/tests/test_modman_start.cpp
+++ server-libs/modman-base/tests/test_modman_start.cpp
@@ -10,26 +10,30 @@ QTEST_MAIN(test_modman_start)
 
 void test_modman_start::emptyModman()
 {
-
     VeinEvent::EventHandler vfEventHandler;
-    VeinTestServer vfTestServer(&vfEventHandler);
-    VfTestComponentAddListener vfListener;
 
-    vfEventHandler.addSubsystem(&vfListener);
+    VfTestComponentAddListener vfAddListener;
+    VfTestComponentChangeListener vfChangeListener;
+    vfEventHandler.addSubsystem(&vfAddListener);
+    vfEventHandler.addSubsystem(&vfChangeListener);
+
+    VeinTestServer vfTestServer(&vfEventHandler);
 
     feedEventLoop();
 
-    VfTestComponentAddListener::AddHash hash = vfListener.getComponentHash();
+    VfTestComponentAddListener::AddHash hash = vfAddListener.getComponentHash();
     QCOMPARE(hash.size(), 1);
     int entityId = 0;
     QCOMPARE(hash[entityId].size(), 7);
     QCOMPARE(hash[entityId]["EntityName"], "_System");
     QCOMPARE(hash[entityId].contains("Session"), true);
     QCOMPARE(hash[entityId].contains("SessionsAvailable"), true);
-    QCOMPARE(hash[entityId]["Entities"], QVariant());
+    QCOMPARE(hash[entityId]["Entities"], QVariant()); // _SYSTEM listed once ModuleManagerController::initializeEntity is called
     QCOMPARE(hash[entityId].contains("ModulesPaused"), true);
     QCOMPARE(hash[entityId].contains("Error_Messages"), true);
     QCOMPARE(hash[entityId].contains("LoggedComponents"), true);
+
+    QCOMPARE(vfChangeListener.getComponentChangeList().size(), 0);
 }
 
 void test_modman_start::feedEventLoop()
