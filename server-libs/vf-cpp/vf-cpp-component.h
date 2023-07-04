56e071b5ae93285b91cae6624e565cbbf7744d64
Andreas Müller <schnitzeltony@gmail.com>
2023-07-04 22:33:57 +0200

    Cleanup cVeinModuleComponent
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/vf-cpp/vf-cpp-component.h
+++ server-libs/vf-cpp/vf-cpp-component.h
@@ -14,44 +14,32 @@
 #include <ve_eventsystem.h>
 
 namespace VfCpp {
-/**
- * @brief The cVeinModuleComponent class
- *
- * Abstraction for vein components
- */
+
+// a vein-only extract of VfModuleComponent
 class cVeinModuleComponent: public QObject
 {
     Q_OBJECT
 public:
-
     typedef  QSharedPointer< cVeinModuleComponent > Ptr;
-
     cVeinModuleComponent(int entityId, VeinEvent::EventSystem *eventsystem, QString name, QVariant initval, bool readOnly=false);
     ~cVeinModuleComponent();
-
     void setValueByEvent(QVariant value);
-
     QVariant getValue();
     QString getName();
-
 signals:
     void sigValueChanged(QVariant); // we connect here if we want to do something on changed values
     void sigValueQuery(QVariant); // we connect here if we want to read a value before returning data from storage ...perhaps with parameter
-
 public slots:
     void setValue(QVariant value); // here we have to emit event for notification
     void setError(); // here we have to emit event for error notification
-
 protected:
     int m_nEntityId;
     VeinEvent::EventSystem *m_pEventSystem;
     QString m_sName;
     QVariant m_vValue;
     bool m_readOnly;
-
 protected:
-
-    virtual void sendNotification(VeinComponent::ComponentData::Command vcmd);
+    void sendNotification(VeinComponent::ComponentData::Command vcmd);
 };
 }
 
