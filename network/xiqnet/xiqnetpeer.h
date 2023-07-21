679dc2e0e3f71e25e824856fdd34fd5bfd7c4c8d
Andreas Müller <schnitzeltony@gmail.com>
2023-07-21 23:49:30 +0200

    TcpPeer/XiQNetPeer: Make visit more fun
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/xiqnet/xiqnetpeer.h
+++ network/xiqnet/xiqnetpeer.h
@@ -3,107 +3,50 @@
 
 #include "xiqnet_global.h"
 #include "xiqnet_export.h"
-
-#include <QObject>
+#include "xiqnetwrapper.h"
 #include <QString>
-#include <QAbstractSocket>
+#include <QTcpSocket>
 
 class XiQNetPeerPrivate;
-class XiQNetWrapper;
-class QTcpSocket;
 
-/**
- * @brief Custom QTcpSocket based implementation of a TCP network peer
- */
 class XIQNET_EXPORT XiQNetPeer : public QObject
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  explicit XiQNetPeer(QObject *t_parent = nullptr);
-  explicit XiQNetPeer(qintptr t_socketDescriptor, QObject *t_parent = nullptr);
+    explicit XiQNetPeer(QObject *t_parent = nullptr);
+    explicit XiQNetPeer(qintptr t_socketDescriptor, QObject *t_parent = nullptr);
+    ~XiQNetPeer();
 
-  ~XiQNetPeer();
+    QString getIpAddress() const;
+    quint16 getPort() const;
+    bool isConnected() const;
 
-  QString getIpAddress() const;
-  quint16 getPort() const;
-  bool isConnected() const;
+    int getPeerId() const;
+    void setPeerId(int t_peerId);
 
+    // This is lazy crap
+    QTcpSocket *getTcpSocket() const;
 
-  int getPeerId() const;
-  void setPeerId(int t_peerId);
+    // This makes the difference to vf-tcp: protobuf en/decoding is in here / vf-tcp
+    // handles opaque QByteArray - see further google below
+    XiQNetWrapper *getWrapper() const;
+    void setWrapper(XiQNetWrapper *value);
 
-  /**
-   * @brief Allows access to the QTcpSocket in case of non protobuf transmission
-   * @return QTcpSocket used by this instance
-   * @todo refactor, as this violates the OOP principle "separation of concerns"
-   */
-  QTcpSocket *getTcpSocket() const;
-
-
-  /**
-   * @brief Returns a reference to the used XiQNetWrapper
-   * @return
-   */
-  XiQNetWrapper *getWrapper() const;
-  /**
-   * @brief Sets the XiQNetWrapper for this peer
-   * @param value
-   */
-  void setWrapper(XiQNetWrapper *value);
-
-  QString getErrorString() const;
-
+    QString getErrorString() const;
 signals:
-  /**
-   * @brief Emitted if successful connected
-   */
-  void sigConnectionEstablished();
-  /**
-   * @brief Emitted when the connection is interrupted
-   */
-  void sigConnectionClosed();
-  /**
-   * @brief Emitted when incoming messages arrive
-   * @param t_Message
-   * @bug Do not use the t_Message parameter with Qt::QueuedConnection as it may be deleted before the slot is called
-   */
-  void sigMessageReceived(std::shared_ptr<google::protobuf::Message> t_Message);
-  /**
-   * @brief Emitted on socket failure
-   * @param t_socketError
-   */
-  void sigSocketError(QAbstractSocket::SocketError t_socketError);
-
+    void sigConnectionEstablished();
+    void sigConnectionClosed();
+    void sigMessageReceived(std::shared_ptr<google::protobuf::Message> t_Message);
+    void sigSocketError(QAbstractSocket::SocketError t_socketError);
 public slots:
-  /**
-   * @brief Transmits the message
-   * @param t_message
-   */
-  void sendMessage(const google::protobuf::Message &t_message) const;
-  /**
-   * @brief Starts the connection in case of a dedicated host connection
-   *
-   * Called after Signal/Slot setup
-   * @param t_ipAddress
-   * @param t_port
-   */
-  void startConnection(QString t_ipAddress, quint16 t_port);
-  /**
-   * @brief Closes the connection
-   */
-  void stopConnection();
+    void sendMessage(const google::protobuf::Message &t_message) const;
+    void startConnection(QString t_ipAddress, quint16 t_port);
+    void closeConnection();
 
-protected slots:
-  /**
-   * @brief Translates incoming messages to google::protobuf::Message via the XiQNetWrapper
-   */
-  void onReadyRead();
-
+private slots:
+    void onReadyRead();
 private:
-  /**
-   * @brief PIMPL pointer
-   */
-  XiQNetPeerPrivate *d_ptr = nullptr;
+    XiQNetPeerPrivate *d_ptr = nullptr;
 };
 
 #endif // PROTOPEER_H
