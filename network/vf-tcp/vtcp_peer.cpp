5118f4565ade64f7cec1ff5771873126274d92ef
Andreas Müller <schnitzeltony@gmail.com>
2023-07-22 14:04:07 +0200

    vtcp_peer: Move disconnect connections to TcpPeerPrivate
    Tested by:
    * Run modulemanager in debug
    * Add a breakpoint to TcpSystem::onClientDisconnected
    * Start & stop vf-declarative-gui => breakpoint reached
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-tcp/vtcp_peer.cpp
+++ network/vf-tcp/vtcp_peer.cpp
@@ -15,7 +15,6 @@ TcpPeer::TcpPeer(qintptr t_socketDescriptor, QObject *t_parent) :
     QObject(t_parent),
     d_ptr(new TcpPeerPrivate(this, t_socketDescriptor))
 {
-    connect(d_ptr->m_tcpSock, &QTcpSocket::disconnected, this, [this](){ emit sigConnectionClosed(this); });
     connect<void(QTcpSocket::*)(QAbstractSocket::SocketError)>(d_ptr->m_tcpSock, &QTcpSocket::error, this, [this](QAbstractSocket::SocketError t_socketError){ emit sigSocketError(this, t_socketError); });
 
     connect(d_ptr->m_tcpSock, &QTcpSocket::disconnected, this, &TcpPeer::closeConnection);
@@ -29,7 +28,7 @@ TcpPeer::TcpPeer(qintptr t_socketDescriptor, QObject *t_parent) :
 void TcpPeer::startConnection(QString t_ipAddress, quint16 t_port)
 {
     d_ptr->startConnection(t_ipAddress, t_port);
-    connect(d_ptr->m_tcpSock, &QTcpSocket::disconnected, this, [this](){ emit sigConnectionClosed(this); });
+
     connect<void(QTcpSocket::*)(QAbstractSocket::SocketError)>(d_ptr->m_tcpSock, &QTcpSocket::error, this, [this](QAbstractSocket::SocketError t_socketError){ emit sigSocketError(this, t_socketError); });
     connect(d_ptr->m_tcpSock, &QTcpSocket::disconnected, this, &TcpPeer::closeConnection);
     d_ptr->m_tcpSock->connectToHost(t_ipAddress, t_port);
