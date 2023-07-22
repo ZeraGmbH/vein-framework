cbfc9d0a50877b58c498c992c65a5b796958e90e
Andreas Müller <schnitzeltony@gmail.com>
2023-07-22 22:14:23 +0200

    vtcp_server: Move startServer functionality to TcpServerPrivate
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-tcp/vtcp_serverprivate.cpp
+++ network/vf-tcp/vtcp_serverprivate.cpp
@@ -1,6 +1,7 @@
 #include "vtcp_serverprivate.h"
 #include "vtcp_peer.h"
 #include "vtcp_server.h"
+#include <systemd/sd-daemon.h>
 
 namespace VeinTcp
 {
@@ -18,5 +19,39 @@ TcpServerPrivate::~TcpServerPrivate()
 void TcpServerPrivate::incomingConnection(qintptr socketDescriptor)
 {
     q_ptr->incomingConnection(socketDescriptor);
+}
+
+bool TcpServerPrivate::startServer(quint16 port, bool systemdSocket)
+{
+    bool retVal = false;
+    if(systemdSocket) {
+        //check if systemd has handed us a socket (service socket activation)
+        int tmpSocketDescriptor = -1;
+        const int socketCount=sd_listen_fds(0);
+        for(int i=0; i<socketCount; ++i) {
+            if(sd_is_socket_inet(SD_LISTEN_FDS_START+i, AF_UNSPEC, SOCK_STREAM, 1, port)) { //(int fd, int family, int type, int listening, uint16_t port)
+                tmpSocketDescriptor = SD_LISTEN_FDS_START+i;
+                break;
+            }
+        }
+
+        if(tmpSocketDescriptor >= SD_LISTEN_FDS_START) {
+            if(setSocketDescriptor(tmpSocketDescriptor)) {
+                retVal = true;
+                qDebug()<<"[vein-tcp] Inherited socket descriptor from systemd, listening on port:" << port;
+            }
+        }
+    }
+
+    if(isListening() == false) {
+        if(listen(QHostAddress::Any, port)) {
+            retVal = true;
+            qDebug()<<"[vein-tcp] Server Started on port:" << port;
+        }
+        else
+            qCritical() << "[vein-tcp] Server could not listen on port:" << port << "error:" << errorString();
+    }
+    return retVal;
+
 }
 }
