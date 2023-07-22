5f175e05a6c10cc48958533ac115ea0b96327dd2
Andreas Müller <schnitzeltony@gmail.com>
2023-07-22 11:57:20 +0200

    TcpPeerPrivate: prettify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-tcp/vtcp_peerprivate.h
+++ network/vf-tcp/vtcp_peerprivate.h
@@ -2,38 +2,24 @@
 #define VEIN_TCP_PEERPRIVATE_H
 
 #include <QUuid>
-
-QT_BEGIN_NAMESPACE
-class QTcpSocket;
-QT_END_NAMESPACE
+#include <QTcpSocket>
 
 namespace VeinTcp
 {
-  class TcpPeer;
+class TcpPeer;
 
-  class TcpPeerPrivate
-  {
-  private:
+class TcpPeerPrivate
+{
+private:
     explicit TcpPeerPrivate(TcpPeer *t_publicPeer);
-
-    /**
-   * @brief readClient
-   * @return
-   */
     QByteArray readArray() const;
-
-    /**
-   * @brief sendByteArray
-   * @param[in] t_byteArray Data that will be sent
-   */
     void sendArray(const QByteArray &t_byteArray) const;
 
     QUuid m_peerId;
-
     QTcpSocket *m_tcpSock = nullptr;
     TcpPeer *q_ptr = nullptr;
 
     friend class TcpPeer;
-  };
+};
 }
 #endif // VEIN_TCP_PEERPRIVATE_H
