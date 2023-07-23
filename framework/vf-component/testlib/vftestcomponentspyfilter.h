e2bffe091d2bed3b4119e18131dd8d0f65f4da30
Andreas Müller <schnitzeltony@gmail.com>
2023-07-23 13:28:23 +0200

    vftestcomponentspyfilter: Add some helping commands - code is not easy to read
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- framework/vf-component/testlib/vftestcomponentspyfilter.h
+++ framework/vf-component/testlib/vftestcomponentspyfilter.h
@@ -6,10 +6,12 @@
 class VfTestComponentSpyFilter
 {
 public:
+    // entityId
     static QList<VfTestComponentSpy::TComponentInfo> filter(QList<VfTestComponentSpy::TComponentInfo> componentList, int entityId);
     static bool hasOne(QList<VfTestComponentSpy::TComponentInfo> componentList, int entityId);
     static VfTestComponentSpy::TComponentInfo first(QList<VfTestComponentSpy::TComponentInfo> componentList, int entityId);
 
+    // componentName
     static QList<VfTestComponentSpy::TComponentInfo> filter(QList<VfTestComponentSpy::TComponentInfo> componentList, QString componentName);
     static bool hasOne(QList<VfTestComponentSpy::TComponentInfo> componentList, QString componentName);
     static VfTestComponentSpy::TComponentInfo first(QList<VfTestComponentSpy::TComponentInfo> componentList, QString componentName);
