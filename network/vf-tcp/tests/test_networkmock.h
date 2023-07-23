9ebe7d0c48dbb7a2f9a26376367e9aae9007f2ea
Andreas Müller <schnitzeltony@gmail.com>
2023-07-23 16:27:36 +0200

    vf-tcp: Add Minimal client/server under test with network
    This can be considered a reference: That is what it needs to establish a
    client/server environment.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ network/vf-tcp/tests/test_networkmock.h
@@ -0,0 +1,18 @@
+#ifndef TEST_NETWORKMOCK_H
+#define TEST_NETWORKMOCK_H
+
+#include <QObject>
+
+class test_networkmock : public QObject
+{
+    Q_OBJECT
+private slots:
+    // We need this to learn howto set up a working syssytem
+    // before starting on network mock. In case we are done
+    // and this test turns fragile, it can be removed
+    void receiveIntrospectionOverRealNetwork();
+private:
+    void feedEventLoop();
+};
+
+#endif // TEST_NETWORKMOCK_H
