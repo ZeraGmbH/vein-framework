c599a979f0be122e413367d4ee61041320bbf1a5
Andreas Müller <schnitzeltony@gmail.com>
2023-07-23 21:04:20 +0200

    Move our initial full vein-stack test to test_veinnetworkstacks
    To create our mock we need smaller environment
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-tcp/tests/test_networkmock.h
+++ network/vf-tcp/tests/test_networkmock.h
@@ -7,11 +7,6 @@ class test_networkmock : public QObject
 {
     Q_OBJECT
 private slots:
-    // We need this to learn howto set up a working syssytem
-    // before starting on network mock. In case we are done
-    // and this test turns fragile, it can be removed
-    void receiveIntrospectionOverRealNetwork();
-    void receiveIntrospectionOverMockNetwork();
 private:
     void feedEventLoop();
 };
