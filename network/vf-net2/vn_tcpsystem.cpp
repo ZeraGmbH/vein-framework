84fad3d4c971798c0c127e6528e018ec419c8246
Andreas Müller <schnitzeltony@gmail.com>
2023-07-23 17:20:55 +0200

    vf-net2: Try to make code a bit more polite
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-net2/vn_tcpsystem.cpp
+++ network/vf-net2/vn_tcpsystem.cpp
@@ -1,8 +1,6 @@
 #include "vn_tcpsystem.h"
-
 #include "vn_protocolevent.h"
 #include "vn_networkstatusevent.h"
-
 #include <vtcp_peer.h>
 #include <vtcp_server.h>
 
@@ -37,7 +35,6 @@ void TcpSystem::connectToServer(const QString &t_host, quint16 t_port)
     VF_ASSERT(t_host.isEmpty() == false, "Empty host");
     VF_ASSERT(t_port > 0, "Port must be > 0");
     vCDebug(VEIN_NET_TCP) << "Attempting connection to:"<< t_host << "on port:" << t_port;
-
     VeinTcp::TcpPeer *tmpPeer = new VeinTcp::TcpPeer(this);
     connect(tmpPeer, &VeinTcp::TcpPeer::sigSocketError, this, &TcpSystem::onSocketError);
     connect(tmpPeer, &VeinTcp::TcpPeer::sigConnectionEstablished, this, &TcpSystem::onConnectionEstablished);
@@ -45,19 +42,17 @@ void TcpSystem::connectToServer(const QString &t_host, quint16 t_port)
     tmpPeer->startConnection(t_host, t_port);
 }
 
-void TcpSystem::onClientConnected(VeinTcp::TcpPeer *t_networkPeer)
+void TcpSystem::onClientConnected(VeinTcp::TcpPeer *peer)
 {
-    Q_ASSERT(t_networkPeer != nullptr);
-
-    connect(t_networkPeer, &VeinTcp::TcpPeer::sigMessageReceived, this, &TcpSystem::onMessageReceived);
-    m_waitingAuth.append(t_networkPeer);
-    connect(t_networkPeer, &VeinTcp::TcpPeer::sigConnectionClosed, this, &TcpSystem::onClientDisconnected);
-
-    t_networkPeer->sendMessage(QByteArray("welcome")); //message content is irrelevant
+    Q_ASSERT(peer != nullptr);
+    connect(peer, &VeinTcp::TcpPeer::sigMessageReceived, this, &TcpSystem::onMessageReceived);
+    m_waitingAuth.append(peer);
+    connect(peer, &VeinTcp::TcpPeer::sigConnectionClosed, this, &TcpSystem::onClientDisconnected);
+    peer->sendMessage(QByteArray("welcome")); //message content is irrelevant
 #ifdef VN2_LEGACY_UNREACHABLE
     /** @todo implement authentication */
     protobuf::VeinProtocol *protoAuth = new protobuf::VeinProtocol();
-    t_networkPeer->sendMessage(protoAuth);
+    peer->sendMessage(protoAuth);
     delete protoAuth;
 #endif // VN2_LEGACY_UNREACHABLE
 }
@@ -65,11 +60,8 @@ void TcpSystem::onClientConnected(VeinTcp::TcpPeer *t_networkPeer)
 void TcpSystem::onConnectionEstablished(VeinTcp::TcpPeer *t_peer)
 {
     Q_ASSERT(t_peer!=nullptr);
-
     m_waitingAuth.append(t_peer);
-
     connect(t_peer, &VeinTcp::TcpPeer::sigMessageReceived, this, &TcpSystem::onMessageReceived);
-
     t_peer->sendMessage(QByteArray("hello")); //message content is irrelevant
 #ifdef VN2_LEGACY_UNREACHABLE
     /** @todo implement authentication */
@@ -82,9 +74,7 @@ void TcpSystem::onConnectionEstablished(VeinTcp::TcpPeer *t_peer)
 void TcpSystem::onConnectionClosed(VeinTcp::TcpPeer *t_peer)
 {
     Q_ASSERT(t_peer!=nullptr);
-
     QUuid tmpPeerId = t_peer->getPeerId();
-
     vCDebug(VEIN_NET_TCP) << "Disconnected from server with ID:" << tmpPeerId;
     m_waitingAuth.removeAll(t_peer);
     m_peerList.remove(tmpPeerId);
@@ -93,12 +83,9 @@ void TcpSystem::onConnectionClosed(VeinTcp::TcpPeer *t_peer)
 void TcpSystem::onClientDisconnected(VeinTcp::TcpPeer *t_peer)
 {
     Q_ASSERT(t_peer!=nullptr);
-
     QUuid tmpPeerId = t_peer->getPeerId();
-
     NetworkStatusEvent *sEvent = new NetworkStatusEvent(NetworkStatusEvent::NetworkStatus::NSE_DISCONNECTED, tmpPeerId);
     emit sigSendEvent(sEvent);
-
     vCDebug(VEIN_NET_TCP) << "Client disconnected with ID:" << tmpPeerId << "sent NetworkStatusEvent:" << sEvent;
     m_waitingAuth.removeAll(t_peer);
     m_peerList.remove(tmpPeerId);
@@ -110,7 +97,6 @@ void TcpSystem::onMessageReceived(VeinTcp::TcpPeer *t_sender, QByteArray t_buffe
 {
     Q_ASSERT(t_buffer.isNull() == false);
     Q_ASSERT(t_sender!=nullptr);
-
     //vCDebug(VEIN_NET_TCP_VERBOSE)  << "Message received:" << proto->DebugString().c_str();
     if(m_waitingAuth.contains(t_sender))
     {
@@ -137,7 +123,6 @@ void TcpSystem::onMessageReceived(VeinTcp::TcpPeer *t_sender, QByteArray t_buffe
 void TcpSystem::onSocketError(VeinTcp::TcpPeer *t_peer, QAbstractSocket::SocketError t_socketError)
 {
     Q_ASSERT(t_peer != nullptr);
-
     NetworkStatusEvent *sEvent = new NetworkStatusEvent(NetworkStatusEvent::NetworkStatus::NSE_SOCKET_ERROR, t_peer->getPeerId());
     sEvent->setError(t_socketError);
     qCCritical(VEIN_NET_TCP) << "Connection error on network with id:" << t_peer->getPeerId() << "error:" << t_peer->getErrorString() << "sent NetworkStatusEvent:" << sEvent;
@@ -147,33 +132,26 @@ void TcpSystem::onSocketError(VeinTcp::TcpPeer *t_peer, QAbstractSocket::SocketE
 void TcpSystem::processEvent(QEvent *t_event)
 {
     Q_ASSERT(t_event != nullptr);
-    if(t_event->type()==ProtocolEvent::getEventType())
-    {
+    if(t_event->type()==ProtocolEvent::getEventType()) {
         ProtocolEvent *pEvent=nullptr;
         pEvent = static_cast<ProtocolEvent *>(t_event);
         Q_ASSERT(pEvent != nullptr);
         /// @todo rework event origin concept
-        //do not process protocol events from foreign systems, that is the job of NetworkSystem
-        if(pEvent->isOfLocalOrigin() == true) //event is outgoing
-        {
+        // do not process protocol events from foreign systems, that is the job of NetworkSystem
+        if(pEvent->isOfLocalOrigin() == true) { //event is outgoing
             //send to all
-            if(pEvent->receivers().isEmpty())
-            {
+            if(pEvent->receivers().isEmpty()) {
                 const auto tmpPeerlist = m_peerList.values();
                 vCDebug(VEIN_NET_TCP_VERBOSE) << "Sending ProtocolEvent" << pEvent << "to receivers:" << tmpPeerlist;// << pEvent->protobuf()->DebugString().c_str();
                 for(VeinTcp::TcpPeer *tmpPeer : tmpPeerlist)
-                {
                     tmpPeer->sendMessage(pEvent->buffer());
-                }
             }
             else //send to all explicit receivers
             {
                 const auto tmpEventReceiversCopy = pEvent->receivers();
-                for(const QUuid &receiverId : tmpEventReceiversCopy)
-                {
+                for(const QUuid &receiverId : tmpEventReceiversCopy) {
                     VeinTcp::TcpPeer *tmpPeer = m_peerList.value(receiverId,0);
-                    if(tmpPeer)
-                    {
+                    if(tmpPeer) {
                         vCDebug(VEIN_NET_TCP_VERBOSE) << "Sending ProtocolEvent" << pEvent << "to receiver:" << tmpPeer;// << pEvent->protobuf()->DebugString().c_str();
                         tmpPeer->sendMessage(pEvent->buffer());
                     }
