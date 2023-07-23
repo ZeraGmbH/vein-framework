2d47bae6c15e4f17ee2c652a545e9aaf445da69a
Andreas Müller <schnitzeltony@gmail.com>
2023-07-23 21:04:20 +0200

    Move our initial full vein-stack test to test_veinnetworkstacks
    To create our mock we need smaller environment
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-tcp/tests/test_networkmock.cpp
+++ network/vf-tcp/tests/test_networkmock.cpp
@@ -1,78 +1,16 @@
 #include "test_networkmock.h"
-#include "vfcommandeventhandlersystem.h"
-#include "vfsentityinsubscription.h"
-#include "veintestserver.h"
-#include "ve_eventhandler.h"
-#include "vn_networksystem.h"
-#include "vn_tcpsystem.h"
-#include "vtcp_workerfactorymethodstest.h"
+#include <QAbstractEventDispatcher>
+#include <QSignalSpy>
+#include <QTest>
 #include <QAbstractEventDispatcher>
 #include <QSignalSpy>
 #include <QTest>
 
 QTEST_MAIN(test_networkmock)
 
-static constexpr int systemEntityId = 0;
-static constexpr int serverPort = 4242;
-
-struct TestServerStack
-{
-    VeinEvent::EventHandler eventHandler;
-    VeinTestServer server;
-    VeinNet::NetworkSystem netSystem;
-    VeinNet::TcpSystem tcpSystem;
-    TestServerStack() :
-        server(&eventHandler)
-    {
-        eventHandler.addSubsystem(&netSystem);
-        eventHandler.addSubsystem(&tcpSystem);
-        tcpSystem.startServer(serverPort, false);
-        while(QCoreApplication::eventDispatcher()->processEvents(QEventLoop::AllEvents));
-    }
-};
-
-struct TestClientStack
-{
-    VeinEvent::EventHandler eventHandler;
-    VeinNet::NetworkSystem netSystem;
-    VeinNet::TcpSystem tcpSystem;
-    TestClientStack()
-    {
-        netSystem.setOperationMode(VeinNet::NetworkSystem::VNOM_PASS_THROUGH); //!!!!!
-        eventHandler.addSubsystem(&netSystem);
-        eventHandler.addSubsystem(&tcpSystem);
-    }
-};
-
-void test_networkmock::receiveIntrospectionOverRealNetwork()
-{
-    VeinTcp::TcpWorkerFactoryMethodsTest::enableProduction();
-    TestServerStack serverStack;
-
-    TestClientStack clientStack;
-    VfCommandEventHandlerSystem cmdEventHandlerSystem;
-    clientStack.eventHandler.addSubsystem(&cmdEventHandlerSystem);
-    VfsEntityInSubscriptionPtr entityToSubscribe = VfsEntityInSubscription::create(systemEntityId);
-    cmdEventHandlerSystem.addItem(entityToSubscribe);
-
-    clientStack.tcpSystem.connectToServer("127.0.0.1", serverPort);
-    QSignalSpy spy(entityToSubscribe.get(), &VfsEntityInSubscription::sigSubscribed);
-    bool clientConnected = false;
-    QObject::connect(&clientStack.tcpSystem, &VeinNet::TcpSystem::sigConnnectionEstablished, [&]() {
-        clientConnected = true;
-        entityToSubscribe->sendSubscrption();
-    });
-    spy.wait(1000);
-    QVERIFY(clientConnected);
-    QCOMPARE(spy.count(), 1);
-}
-
-void test_networkmock::receiveIntrospectionOverMockNetwork()
-{
-
-}
 
 void test_networkmock::feedEventLoop()
 {
     while(QCoreApplication::eventDispatcher()->processEvents(QEventLoop::AllEvents));
 }
+
---
+++ network/vf-tcp/tests/test_veinnetworkstacks.h
@@ -0,0 +1,19 @@
+#ifndef TEST_VEINNETWORKSTACKS_H
+#define TEST_VEINNETWORKSTACKS_H
+
+#include <QObject>
+
+class test_veinnetworkstacks : public QObject
+{
+    Q_OBJECT
+private slots:
+    // We need this to learn howto set up a working syssytem
+    // before starting on network mock. In case we are done
+    // and this test turns fragile, it can be removed
+    void receiveIntrospectionOverRealNetwork();
+    void receiveIntrospectionOverMockNetwork();
+private:
+    void feedEventLoop();
+};
+
+#endif // TEST_VEINNETWORKSTACKS_H
