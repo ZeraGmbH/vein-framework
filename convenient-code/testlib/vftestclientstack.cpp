68d225fcf2a1203a5f866b45b477d8f2f7074d10
Andreas Müller <schnitzeltony@gmail.com>
2023-07-27 21:33:12 +0200

    Introduce VfTestClientStack::subscribeEntityId
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- convenient-code/testlib/vftestclientstack.cpp
+++ convenient-code/testlib/vftestclientstack.cpp
@@ -1,8 +1,16 @@
 #include "vftestclientstack.h"
+#include "vfsimpleentitysubscriber.h"
 
 VfTestClientStack::VfTestClientStack()
 {
     netSystem.setOperationMode(VeinNet::NetworkSystem::VNOM_PASS_THROUGH); //!!!!!
     eventHandler.addSubsystem(&netSystem);
     eventHandler.addSubsystem(&tcpSystem);
+}
+
+void VfTestClientStack::subscribeEntityId(int entityId, VfCommandEventHandlerSystem *cmdEventHandlerSystem)
+{
+    VfSimpleEntitySubscriberPtr entityToSubscribe = VfSimpleEntitySubscriber::create(entityId);
+    cmdEventHandlerSystem->addItem(entityToSubscribe);
+    entityToSubscribe->sendSubscrption();
 }
