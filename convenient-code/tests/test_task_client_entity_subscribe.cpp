4087b68b1b31c98fdb2bddd4c93e4aa67328adce
Andreas Müller <schnitzeltony@gmail.com>
2023-08-01 15:03:43 +0200

    Introduce TaskClientEntitySubscribe
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ convenient-code/tests/test_task_client_entity_subscribe.cpp
@@ -0,0 +1,92 @@
+#include "test_task_client_entity_subscribe.h"
+#include "taskcliententitysubscribe.h"
+#include "vfcorestackclient.h"
+#include "vftestserverstack.h"
+#include "vtcp_workerfactorymethodstest.h"
+#include "timerfactoryqtfortest.h"
+#include "timemachinefortest.h"
+#include <QAbstractEventDispatcher>
+#include <QSignalSpy>
+#include <QTest>
+
+QTEST_MAIN(test_task_client_entity_subscribe)
+
+static constexpr int serverPort = 4242;
+static constexpr int systemEntityId = 0;
+static constexpr int invalidEntityId = 1;
+static constexpr int stdTimeout = 1000;
+
+void test_task_client_entity_subscribe::init()
+{
+    TimerFactoryQtForTest::enableTest();
+    TimeMachineForTest::reset();
+}
+
+void test_task_client_entity_subscribe::subscibeOk()
+{
+    VeinTcp::TcpWorkerFactoryMethodsTest::enableMockNetwork();
+    VfTestServerStack serverStack(serverPort);
+
+    VfCoreStackClient clientStack;
+    clientStack.tcpSystem.connectToServer("127.0.0.1", serverPort);
+    feedEventLoop();
+
+    std::shared_ptr<QStringList> components = std::make_shared<QStringList>();
+    TaskClientEntitySubscribe task(systemEntityId, clientStack.cmdEventHandlerSystem, components);
+    QSignalSpy spy(&task, &TaskTemplate::sigFinish);
+    task.start();
+    feedEventLoop();
+    QCOMPARE(spy.count(), 1);
+    QVERIFY(components->size() > 0);
+}
+
+void test_task_client_entity_subscribe::timeout()
+{
+    VfCoreStackClient clientStack;
+    feedEventLoop();
+
+    std::shared_ptr<QStringList> components = std::make_shared<QStringList>();
+    TaskTemplatePtr task = TaskClientEntitySubscribe::create(systemEntityId, clientStack.cmdEventHandlerSystem, components, stdTimeout);
+
+    bool receivedOk = true;
+    int timeout=0;
+    connect(task.get(), &TaskTemplate::sigFinish, [&](bool ok, int taskId) {
+        Q_UNUSED(taskId)
+        receivedOk = ok;
+        timeout = TimeMachineForTest::getInstance()->getCurrentTimeMs();
+    });
+    task->start();
+    TimeMachineForTest::getInstance()->processTimers(2*stdTimeout);
+    QVERIFY(!receivedOk);
+    QCOMPARE(timeout, stdTimeout);
+}
+
+void test_task_client_entity_subscribe::invalidEntity()
+{
+    VeinTcp::TcpWorkerFactoryMethodsTest::enableMockNetwork();
+    VfTestServerStack serverStack(serverPort);
+
+    VfCoreStackClient clientStack;
+    clientStack.tcpSystem.connectToServer("127.0.0.1", serverPort);
+    feedEventLoop();
+
+    std::shared_ptr<QStringList> components = std::make_shared<QStringList>();
+    TaskTemplatePtr task = TaskClientEntitySubscribe::create(invalidEntityId, clientStack.cmdEventHandlerSystem, components, stdTimeout);
+
+    bool receivedOk = true;
+    int timeout=0;
+    connect(task.get(), &TaskTemplate::sigFinish, [&](bool ok, int taskId) {
+        Q_UNUSED(taskId)
+        receivedOk = ok;
+        timeout = TimeMachineForTest::getInstance()->getCurrentTimeMs();
+    });
+    task->start();
+    TimeMachineForTest::getInstance()->processTimers(2*stdTimeout);
+    QVERIFY(!receivedOk);
+    QCOMPARE(timeout, stdTimeout);
+}
+
+void test_task_client_entity_subscribe::feedEventLoop()
+{
+    while(QCoreApplication::eventDispatcher()->processEvents(QEventLoop::AllEvents));
+}
---
+++ convenient-code/tests/test_task_client_entity_subscribe.h
@@ -0,0 +1,18 @@
+#ifndef TEST_TASK_CLIENT_ENTITY_SUBSCRIBE_H
+#define TEST_TASK_CLIENT_ENTITY_SUBSCRIBE_H
+
+#include <QObject>
+
+class test_task_client_entity_subscribe : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void subscibeOk();
+    void timeout();
+    void invalidEntity();
+private:
+    void feedEventLoop();
+};
+
+#endif // TEST_TASK_CLIENT_ENTITY_SUBSCRIBE_H
