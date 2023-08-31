27ce1c44bbf49cdd3d29f79e32128f157439ab73
Andreas Müller <schnitzeltony@gmail.com>
2023-08-31 22:13:53 +0200

    convenient-code: Rework ContainerSafeDeleteWhileLoop typedefs for better readability
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- convenient-code/lib/command-event-handler/vf_cmd_event_item.h
+++ convenient-code/lib/command-event-handler/vf_cmd_event_item.h
@@ -3,7 +3,12 @@
 
 #include "ve_eventsystem.h"
 #include "ve_commandevent.h"
+#include "container_safe_delete_while_loop.h"
+#include <unordered_set>
 #include <memory>
+
+class VfCmdEventItem;
+typedef std::shared_ptr<VfCmdEventItem> VfCmdEventItemPtr;
 
 // Base item handled by VfCmdEventHandler
 class VfCmdEventItem
@@ -16,10 +21,10 @@ public:
     virtual void processCommandEvent(VeinEvent::CommandEvent *cmdEvent) = 0;
     virtual void processErrorCommandEventData(VeinEvent::EventData *originalEventData) = 0;
 private:
+    typedef ContainerSafeDeleteWhileLoop<VfCmdEventItemPtr, std::unordered_set<VfCmdEventItemPtr>> UnorderedSetSafeDelete;
     const int m_entityId;
     VeinEvent::EventSystem* m_eventSystem = nullptr;
 };
 
-typedef std::shared_ptr<VfCmdEventItem> VfCmdEventItemPtr;
 
 #endif // VFCMDEVENTITEM_H
