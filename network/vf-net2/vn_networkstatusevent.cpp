0303adf670dbdf0e295fe5b078233bc3b8a88a19
Andreas Müller <schnitzeltony@gmail.com>
2023-11-01 15:06:00 +0100

    vf-net2: Indentation & sequence
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/vf-net2/vn_networkstatusevent.cpp
+++ network/vf-net2/vn_networkstatusevent.cpp
@@ -2,39 +2,39 @@
 
 namespace VeinNet
 {
-  NetworkStatusEvent::NetworkStatusEvent(NetworkStatus t_status, QUuid t_peerId) :
+const int NetworkStatusEvent::s_eventType = QEvent::registerEventType();
+
+NetworkStatusEvent::NetworkStatusEvent(NetworkStatus t_status, QUuid t_peerId) :
     QEvent(static_cast<QEvent::Type>(getEventType())),
     m_status(t_status),
     m_socketError(),
     m_peerId(t_peerId)
-  {
+{
     this->setAccepted(false);
-  }
+}
 
-  int NetworkStatusEvent::getEventType()
-  {
+int NetworkStatusEvent::getEventType()
+{
     return s_eventType;
-  }
+}
 
-  NetworkStatusEvent::NetworkStatus NetworkStatusEvent::getStatus() const
-  {
+NetworkStatusEvent::NetworkStatus NetworkStatusEvent::getStatus() const
+{
     return m_status;
-  }
+}
 
-  void NetworkStatusEvent::setError(QAbstractSocket::SocketError t_socketError)
-  {
+void NetworkStatusEvent::setError(QAbstractSocket::SocketError t_socketError)
+{
     m_socketError = t_socketError;
-  }
+}
 
-  QAbstractSocket::SocketError NetworkStatusEvent::getError() const
-  {
+QAbstractSocket::SocketError NetworkStatusEvent::getError() const
+{
     return m_socketError;
-  }
-  QUuid NetworkStatusEvent::getPeerId() const
-  {
+}
+QUuid NetworkStatusEvent::getPeerId() const
+{
     return m_peerId;
-  }
+}
 
-
-  const int NetworkStatusEvent::s_eventType = QEvent::registerEventType();
 } // namespace VeinNet
