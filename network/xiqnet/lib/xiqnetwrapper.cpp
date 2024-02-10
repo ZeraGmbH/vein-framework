f1e43c621139c231e406c5f7c85fd08e2ec6c71f
Andreas Müller <schnitzeltony@gmail.com>
2024-02-10 22:29:06 +0100

    Make XiQNetWrapper readable
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/xiqnet/lib/xiqnetwrapper.cpp
+++ network/xiqnet/lib/xiqnetwrapper.cpp
@@ -4,8 +4,7 @@
 std::shared_ptr<google::protobuf::Message> XiQNetWrapper::byteArrayToProtobuf(QByteArray t_byteArray)
 {
     ProtobufMessage::NetMessage *intermediate = new ProtobufMessage::NetMessage();
-    if(!intermediate->ParseFromArray(t_byteArray, t_byteArray.size()))
-    {
+    if(!intermediate->ParseFromArray(t_byteArray, t_byteArray.size())) {
         ProtobufMessage::NetMessage::ScpiCommand *cmd = intermediate->mutable_scpi();
         cmd->set_command(t_byteArray.data(), t_byteArray.size());
     }
--- network/xiqnet/lib/xiqnetwrapper.h
+++ network/xiqnet/lib/xiqnetwrapper.h
@@ -1,42 +1,15 @@
 #ifndef PROTOWRAPPER_H
 #define PROTOWRAPPER_H
 
-#include "xiqnet_global.h"
 #include <QByteArray>
+#include <memory>
+#include <google/protobuf/message.h>
 
-namespace google
-{
-  namespace protobuf
-  {
-    class Message;
-  }
-}
-
-
-/**
- * @brief A conveniant wrapper class for custom protobuf based classes
- *
- * It has to be implemented to use the "user specific" protocol buffers
- *
- * Often user specific overrides are not necessary because our implentations
- * match most clients' needs.
- */
 class XiQNetWrapper
 {
 public:
-    virtual ~XiQNetWrapper() {}
-    /**
-     * @brief Parses a QByteArray to create a google::protobuf::Message
-     * @param t_byteArray
-     * @return
-     */
+    virtual ~XiQNetWrapper() = default;
     virtual std::shared_ptr<google::protobuf::Message> byteArrayToProtobuf(QByteArray t_byteArray);
-
-    /**
-    * @brief Serializes a protobuf message to QByteArray
-    * @param t_protobufMessage
-    * @return
-    */
     virtual QByteArray protobufToByteArray(const google::protobuf::Message &t_protobufMessage);
 };
 
