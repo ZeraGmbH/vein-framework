3ece56901ec5a9648722fd6ea961a90ffbd4fb8b
Andreas Müller <schnitzeltony@gmail.com>
2024-02-10 21:18:14 +0100

    xiqnetpeerprivate.h: Make readable
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- network/xiqnet/lib/xiqnetpeerprivate.h
+++ network/xiqnet/lib/xiqnetpeerprivate.h
@@ -9,38 +9,21 @@ class XiQNetPeer;
 
 class QTcpSocket;
 
-/**
- * @brief XiQNetPeer PIMPL
- */
 class XiQNetPeerPrivate
 {
 private:
-  explicit XiQNetPeerPrivate(XiQNetPeer *t_publicPeer);
+    explicit XiQNetPeerPrivate(XiQNetPeer *t_publicPeer);
 
-  /**
-   * @brief readClient
-   * @return
-   */
-  QByteArray readArray();
+    QByteArray readArray();
+    void sendArray(const QByteArray &t_byteArray) const;
+    int m_peerId = -1;
 
-  /**
-   * @brief sendByteArray
-   * @param[in] t_byteArray Data that will be sent
-   */
-  void sendArray(const QByteArray &t_byteArray) const;
+    qint32 m_socketBufferExpectedSize = 0;
+    QTcpSocket *m_tcpSock = nullptr;
+    XiQNetWrapper *m_wrapper = nullptr;
+    XiQNetPeer *q_ptr = nullptr;
 
-  int m_peerId = -1;
-
-  /**
-   * @brief when a tcp message is split in multiple fragments this will ensure that all fragments are complete
-   * expected size in bytes
-   */
-  qint32 m_socketBufferExpectedSize = 0;
-  QTcpSocket *m_tcpSock = nullptr;
-  XiQNetWrapper *m_wrapper = nullptr;
-  XiQNetPeer *q_ptr = nullptr;
-
-  friend class XiQNetPeer;
+    friend class XiQNetPeer;
 };
 
 #endif // PROTOPEERPRIVATE_H
