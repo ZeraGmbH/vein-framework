9d8203af2753c7bf1a75a8d30b21547812de3f38
Andreas Müller <schnitzeltony@gmail.com>
2024-03-27 15:16:52 +0100

    testlicensesystem.h: Remove unneeded virtuals
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/testlib/testlicensesystem.h
+++ server-libs/modman-base/testlib/testlicensesystem.h
@@ -7,9 +7,9 @@ class TestLicenseSystem : public LicenseSystemInterface
 {
     Q_OBJECT
 public:
-    virtual bool isSystemLicensed(const QString &uniqueModuleName) override;
-    virtual bool serialNumberIsInitialized() const  override;
-    virtual void processEvent(QEvent *event) override;
+    bool isSystemLicensed(const QString &uniqueModuleName) override;
+    bool serialNumberIsInitialized() const  override;
+    void processEvent(QEvent *event) override;
 };
 
 #endif // TESTLICENSESYSTEM_H
