6d4a23e03f31bb23a2f42367e4bf80356849ffec
Andreas Müller <schnitzeltony@gmail.com>
2024-03-27 14:57:41 +0100

    Rename LicenseSystemMock by TestLicenseSystem
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/testlib/testlicensesystem.h
+++ server-libs/modman-base/testlib/testlicensesystem.h
@@ -1,16 +1,15 @@
-#ifndef LICENSESYSTEMMOCK_H
-#define LICENSESYSTEMMOCK_H
+#ifndef TESTLICENSESYSTEM_H
+#define TESTLICENSESYSTEM_H
 
 #include <licensesysteminterface.h>
 
-class LicenseSystemMock : public LicenseSystemInterface
+class TestLicenseSystem : public LicenseSystemInterface
 {
     Q_OBJECT
 public:
-    explicit LicenseSystemMock();
     virtual bool isSystemLicensed(const QString &uniqueModuleName) override;
     virtual bool serialNumberIsInitialized() const  override;
     virtual void processEvent(QEvent *event) override;
 };
 
-#endif // LICENSESYSTEMMOCK_H
+#endif // TESTLICENSESYSTEM_H
