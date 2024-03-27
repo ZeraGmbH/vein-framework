818235fddac876e3e9dce5c8890f56c46b7cc0d2
Andreas Müller <schnitzeltony@gmail.com>
2024-03-27 16:47:19 +0100

    TestVeinServerWithNet: Allow injection of event systems above network systems
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/testlib/testveinserverwithnet.cpp
+++ server-libs/modman-base/testlib/testveinserverwithnet.cpp
@@ -1,11 +1,13 @@
 #include "testveinserverwithnet.h"
 
-TestVeinServerWithNet::TestVeinServerWithNet(int serverPort, bool systemdSocket) :
+TestVeinServerWithNet::TestVeinServerWithNet(int serverPort, QList<VeinEvent::EventSystem *> aboveNetworkSystems) :
     m_server(&m_eventHandler)
 {
+    for(const auto system : qAsConst(aboveNetworkSystems))
+        m_eventHandler.addSubsystem(system);
     m_eventHandler.addSubsystem(&m_netSystem);
     m_eventHandler.addSubsystem(&m_tcpSystem);
-    m_tcpSystem.startServer(serverPort, systemdSocket);
+    m_tcpSystem.startServer(serverPort, false);
 }
 
 VeinEvent::EventHandler *TestVeinServerWithNet::getEventHandler()
