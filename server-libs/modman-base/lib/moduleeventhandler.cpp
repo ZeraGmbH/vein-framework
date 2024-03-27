94fa8484b610d75f34d53646083c0010544d1c6b
Andreas Müller <schnitzeltony@gmail.com>
2024-03-27 12:17:51 +0100

    moduleeventhandler: Get rid of priority arbitration system
    * Nobody asked for it
    * Nobody knows how it works
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/lib/moduleeventhandler.cpp
+++ server-libs/modman-base/lib/moduleeventhandler.cpp
@@ -3,13 +3,6 @@
 
 ModuleEventHandler::ModuleEventHandler(QObject *parent) : VeinEvent::EventHandler(parent)
 {
-
-}
-
-void ModuleEventHandler::setArbitrationSystem(VeinEvent::EventSystem *t_arbitrationSystem)
-{
-    m_arbitration = t_arbitrationSystem;
-    t_arbitrationSystem->attach(this);
 }
 
 void ModuleEventHandler::addSystem(VeinEvent::EventSystem *t_eventSystem)
@@ -28,18 +21,7 @@ void ModuleEventHandler::clearSystems()
 
 void ModuleEventHandler::customEvent(QEvent *t_event)
 {
-    /** @todo maybe event processing can be accelerated with QtConcurrent? */
-
-#ifdef DEVICE_ARBITRATION
-    Q_ASSERT(m_arbitration != nullptr)
-    m_arbitration->processEvent(t_event);
-#endif
-
     for(int i=0; i < m_moduleSystems.count() && t_event->isAccepted()==false; ++i)
-    {
         m_moduleSystems.at(i)->processEvent(t_event);
-    }
-
     VeinEvent::EventHandler::customEvent(t_event);
 }
-
