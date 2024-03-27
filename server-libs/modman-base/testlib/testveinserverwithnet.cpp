65b1177f2d280d5ac66da39470158a0cdf0a6a5c
Andreas Müller <schnitzeltony@gmail.com>
2024-03-27 15:33:30 +0100

    TestVeinServerWithNet: Fix parameter name to systemdSocket
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/testlib/testveinserverwithnet.cpp
+++ server-libs/modman-base/testlib/testveinserverwithnet.cpp
@@ -1,11 +1,11 @@
 #include "testveinserverwithnet.h"
 
-TestVeinServerWithNet::TestVeinServerWithNet(int serverPort, bool systemdScok) :
+TestVeinServerWithNet::TestVeinServerWithNet(int serverPort, bool systemdSocket) :
     m_server(&m_eventHandler)
 {
     m_eventHandler.addSubsystem(&m_netSystem);
     m_eventHandler.addSubsystem(&m_tcpSystem);
-    m_tcpSystem.startServer(serverPort, systemdScok);
+    m_tcpSystem.startServer(serverPort, systemdSocket);
 }
 
 VeinEvent::EventHandler *TestVeinServerWithNet::getEventHandler()
