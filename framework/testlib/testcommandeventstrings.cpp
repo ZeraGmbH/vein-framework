916d6b18cb32e5f0041c8f1c8cd92fb1eff46da8
Andreas Müller <schnitzeltony@gmail.com>
2024-03-28 23:00:24 +0100

    Introduce a vein command event test spy
    Testing clients is almost a mission impossible...
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ framework/testlib/testcommandeventstrings.cpp
@@ -0,0 +1,101 @@
+#include "testcommandeventstrings.h"
+
+using namespace VeinEvent;
+using namespace VeinComponent;
+
+QString TestCommandEventStrings::strSubtype(VeinEvent::CommandEvent::EventSubtype subtype)
+{
+    QString str;
+    switch(subtype) {
+    case CommandEvent::EventSubtype::NOTIFICATION:
+        str = "NOTIFICATION";
+        break;
+    case CommandEvent::EventSubtype::TRANSACTION:
+        str = "TRANSACTION";
+        break;
+    }
+    return str;
+}
+
+QString TestCommandEventStrings::strOrigin(qint8 origin)
+{
+    QString str;
+    switch(origin) {
+    case qint8(EventData::EventOrigin::EO_LOCAL):
+        str = "EO_LOCAL";
+        break;
+    case qint8(EventData::EventOrigin::EO_FOREIGN):
+        str = "EO_FOREIGN";
+        break;
+    default:
+        str = QString().arg("EO_USER_DEFINED + %1", origin - qint8(EventData::EventOrigin::EO_USER_DEFINED));
+        break;
+    }
+    return str;
+}
+
+QString TestCommandEventStrings::strTarget(qint8 target)
+{
+    QString str;
+    switch(target) {
+    case qint8(EventData::EventTarget::ET_IRRELEVANT):
+        str = "ET_IRRELEVANT";
+        break;
+    case qint8(EventData::EventTarget::ET_LOCAL):
+        str = "ET_LOCAL";
+        break;
+    case qint8(EventData::EventTarget::ET_ALL):
+        str = "ET_ALL";
+        break;
+    default:
+        str = QString().arg("ET_USER_DEFINED + %1", target - qint8(EventData::EventTarget::ET_USER_DEFINED));
+        break;
+    }
+    return str;
+}
+
+QString TestCommandEventStrings::strEntityCommand(VeinComponent::EntityData::Command cmd)
+{
+    QString str;
+    switch(cmd) {
+    case EntityData::Command::ECMD_INVALID:
+        str = "ECMD_INVALID";
+        break;
+    case EntityData::Command::ECMD_ADD:
+        str = "ECMD_ADD";
+        break;
+    case EntityData::Command::ECMD_REMOVE:
+        str = "ECMD_REMOVE";
+        break;
+    case EntityData::Command::ECMD_SUBSCRIBE:
+        str = "ECMD_SUBSCRIBE";
+        break;
+    case EntityData::Command::ECMD_UNSUBSCRIBE:
+        str = "ECMD_UNSUBSCRIBE";
+        break;
+    }
+    return str;
+}
+
+QString TestCommandEventStrings::strComponentCommand(VeinComponent::ComponentData::Command cmd)
+{
+    QString str;
+    switch(cmd) {
+    case ComponentData::Command::CCMD_INVALID:
+        str = "CCMD_INVALID";
+        break;
+    case ComponentData::Command::CCMD_ADD:
+        str = "CCMD_ADD";
+        break;
+    case ComponentData::Command::CCMD_REMOVE:
+        str = "CCMD_REMOVE";
+        break;
+    case ComponentData::Command::CCMD_SET:
+        str = "CCMD_SET";
+        break;
+    case ComponentData::Command::CCMD_FETCH:
+        str = "CCMD_FETCH";
+        break;
+    }
+    return str;
+}
--- framework/tests/test_client_command_events.cpp
+++ framework/tests/test_client_command_events.cpp
@@ -2,6 +2,7 @@
 #include "testcommandeventspyeventsystem.h"
 #include "testveinserverwithnet.h"
 #include "vf_core_stack_client.h"
+#include "task_client_component_fetcher.h"
 #include "vtcp_workerfactorymethodstest.h"
 #include <timemachineobject.h>
 #include <QTest>
@@ -9,6 +10,8 @@
 QTEST_MAIN(test_client_command_events)
 
 static constexpr int serverPort = 4242;
+static constexpr int systemEntityId = 0;
+static constexpr int stdTimeout = 1000;
 
 void test_client_command_events::initTestCase()
 {
@@ -21,17 +24,54 @@ void test_client_command_events::subscribeSystemEntity()
     serverNet.getServer()->simulAllModulesLoaded(QString("foo"), QStringList() << "fooList");
 
     VfCoreStackClient client;
-    TestCommandEventSpyEventSystem cmdEventSpy;
+    QJsonObject jsonEvents;
+    TestCommandEventSpyEventSystem cmdEventSpy(&jsonEvents);
     client.appendEventSystem(&cmdEventSpy);
     client.connectToServer("127.0.0.1", serverPort);
     TimeMachineObject::feedEventLoop();
 
     QVERIFY(cmdEventSpy.isEmpty());
 
-    client.subscribeEntity(0);
+    client.subscribeEntity(systemEntityId);
     TimeMachineObject::feedEventLoop();
 
-    QFile file(":/dumpSubscribe.json");
+    QFile file(":/dumpEventsSubscribe.json");
+    QVERIFY(file.open(QFile::ReadOnly));
+    QByteArray jsonExpected = file.readAll();
+    QByteArray jsonDumped = cmdEventSpy.dumpToJson();
+    if(jsonExpected != jsonDumped) {
+        qWarning("Expected events:");
+        qInfo("%s", qPrintable(jsonExpected));
+        qWarning("Dumped events:");
+        qInfo("%s", qPrintable(jsonDumped));
+        QCOMPARE(jsonExpected, jsonDumped);
+    }
+}
+
+void test_client_command_events::fetchSystemEntity()
+{
+    TestVeinServerWithNet serverNet(serverPort);
+    serverNet.getServer()->simulAllModulesLoaded(QString("foo"), QStringList() << "fooList");
+
+    VfCoreStackClient client;
+    VfCmdEventItemEntityPtr entityItem = VfEntityComponentEventItem::create(systemEntityId);
+    client.addItem(entityItem);
+    QJsonObject jsonEvents;
+    TestCommandEventSpyEventSystem cmdEventSpy(&jsonEvents);
+    client.appendEventSystem(&cmdEventSpy);
+    client.connectToServer("127.0.0.1", serverPort);
+    TimeMachineObject::feedEventLoop();
+
+    client.subscribeEntity(systemEntityId);
+    TimeMachineObject::feedEventLoop();
+    cmdEventSpy.clear();
+
+    std::shared_ptr<QVariant> value = std::make_shared<QVariant>();
+    TaskTemplatePtr fetcherTask = TaskClientComponentFetcher::create("EntityName", entityItem, value, stdTimeout);
+    fetcherTask->start();
+    TimeMachineObject::feedEventLoop();
+
+    QFile file(":/dumpEventsFetch.json");
     QVERIFY(file.open(QFile::ReadOnly));
     QByteArray jsonExpected = file.readAll();
     QByteArray jsonDumped = cmdEventSpy.dumpToJson();
