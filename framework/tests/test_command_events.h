ade39418269064aac2b4049d2725fc5f690fb6a4
Andreas Müller <schnitzeltony@gmail.com>
2024-03-29 18:20:40 +0100

    test_command_events: Cleanup further
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- framework/tests/test_command_events.h
+++ framework/tests/test_command_events.h
@@ -4,6 +4,7 @@
 #include "testveinserverwithnet.h"
 #include "vf_core_stack_client.h"
 #include "vf_entity_component_event_item.h"
+#include "testcommandeventspyeventsystem.h"
 #include <memory>
 
 class test_command_events : public QObject
@@ -16,10 +17,13 @@ private slots:
     void init();
     void cleanup();
 private:
+    void setupSpy(QJsonObject &jsonEvents);
     void subscribeClient(int entityId);
     std::unique_ptr<TestVeinServerWithNet> m_netServer;
     std::unique_ptr<VfCoreStackClient> m_netClient;
     VfCmdEventItemEntityPtr m_entityItem;
+    std::unique_ptr<TestCommandEventSpyEventSystem> m_clientCmdEventSpy;
+    std::unique_ptr<TestCommandEventSpyEventSystem> m_serverCmdEventSpy;
 };
 
 #endif // TEST_CLIENT_COMMAND_EVENTS_H
