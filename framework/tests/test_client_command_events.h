cc54722fc51a8d41e55b4f1b162156879e21cdb4
Andreas Müller <schnitzeltony@gmail.com>
2024-03-29 13:45:59 +0100

    test_client_command_events: Setup server once in init()
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- framework/tests/test_client_command_events.h
+++ framework/tests/test_client_command_events.h
@@ -1,15 +1,20 @@
 #ifndef TEST_CLIENT_COMMAND_EVENTS_H
 #define TEST_CLIENT_COMMAND_EVENTS_H
 
-#include <QObject>
+#include "testveinserverwithnet.h"
+#include <memory>
 
 class test_client_command_events : public QObject
 {
     Q_OBJECT
 private slots:
     void initTestCase();
+    void init();
+    void cleanup();
     void subscribeSystemEntity();
     void fetchSystemEntity();
+private:
+    std::unique_ptr<TestVeinServerWithNet> m_netServer;
 };
 
 #endif // TEST_CLIENT_COMMAND_EVENTS_H
