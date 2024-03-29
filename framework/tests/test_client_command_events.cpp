08636ae48d4da95e51ad8d7732ba569480983ace
Andreas Müller <schnitzeltony@gmail.com>
2024-03-29 14:55:57 +0100

    test_client_command_events: More cleanups
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- framework/tests/test_client_command_events.cpp
+++ framework/tests/test_client_command_events.cpp
@@ -1,7 +1,6 @@
 #include "test_client_command_events.h"
 #include "testcommandeventspyeventsystem.h"
 #include "testdumpreporter.h"
-#include "vf_core_stack_client.h"
 #include "task_client_component_fetcher.h"
 #include "vtcp_workerfactorymethodstest.h"
 #include <timemachineobject.h>
@@ -9,40 +8,18 @@
 
 QTEST_MAIN(test_client_command_events)
 
-static constexpr int serverPort = 4242;
 static constexpr int systemEntityId = 0;
 static constexpr int stdTimeout = 1000;
 
-void test_client_command_events::initTestCase()
-{
-    VeinTcp::TcpWorkerFactoryMethodsTest::enableMockNetwork();
-}
-
-void test_client_command_events::init()
-{
-    m_netServer = std::make_unique<TestVeinServerWithNet>(serverPort);
-    m_netServer->getServer()->simulAllModulesLoaded(QString("foo"), QStringList() << "fooList");
-}
-
-void test_client_command_events::cleanup()
-{
-    m_netServer = nullptr;
-}
-
 void test_client_command_events::subscribeSystemEntity()
 {
-    VfCoreStackClient vfClient;
-    vfClient.connectToServer("127.0.0.1", serverPort);
-    TimeMachineObject::feedEventLoop();
-
     QJsonObject jsonEvents;
     TestCommandEventSpyEventSystem serverCmdEventSpy(&jsonEvents, "server");
     m_netServer->getServer()->appendEventSystem(&serverCmdEventSpy);
     TestCommandEventSpyEventSystem clientCmdEventSpy(&jsonEvents, "client");
-    vfClient.appendEventSystem(&clientCmdEventSpy);
+    m_netClient->appendEventSystem(&clientCmdEventSpy);
 
-    vfClient.subscribeEntity(systemEntityId);
-    TimeMachineObject::feedEventLoop();
+    subscribeClient(systemEntityId);
 
     QFile file(":/dumpEventsSubscribe.json");
     QVERIFY(file.open(QFile::ReadOnly));
@@ -53,23 +30,16 @@ void test_client_command_events::subscribeSystemEntity()
 
 void test_client_command_events::fetchSystemEntityComponent()
 {
-    VfCoreStackClient vfClient;
-    VfCmdEventItemEntityPtr entityItem = VfEntityComponentEventItem::create(systemEntityId);
-    vfClient.addItem(entityItem);
-    vfClient.connectToServer("127.0.0.1", serverPort);
-    TimeMachineObject::feedEventLoop();
-
-    vfClient.subscribeEntity(systemEntityId);
-    TimeMachineObject::feedEventLoop();
+    subscribeClient(systemEntityId);
 
     QJsonObject jsonEvents;
     TestCommandEventSpyEventSystem serverCmdEventSpy(&jsonEvents, "server");
     m_netServer->getServer()->appendEventSystem(&serverCmdEventSpy);
     TestCommandEventSpyEventSystem clientCmdEventSpy(&jsonEvents, "client");
-    vfClient.appendEventSystem(&clientCmdEventSpy);
+    m_netClient->appendEventSystem(&clientCmdEventSpy);
 
     std::shared_ptr<QVariant> value = std::make_shared<QVariant>();
-    TaskTemplatePtr fetcherTask = TaskClientComponentFetcher::create("EntityName", entityItem, value, stdTimeout);
+    TaskTemplatePtr fetcherTask = TaskClientComponentFetcher::create("EntityName", m_entityItem, value, stdTimeout);
     fetcherTask->start();
     TimeMachineObject::feedEventLoop();
     QCOMPARE(*value, "_System");
@@ -79,4 +49,35 @@ void test_client_command_events::fetchSystemEntityComponent()
     QByteArray jsonExpected = file.readAll();
     QByteArray jsonDumped = TestDumpReporter::dump(jsonEvents);
     QVERIFY(TestDumpReporter::reportOnFail(jsonExpected, jsonDumped));
+}
+
+void test_client_command_events::initTestCase()
+{
+    VeinTcp::TcpWorkerFactoryMethodsTest::enableMockNetwork();
+}
+
+void test_client_command_events::init()
+{
+    constexpr int serverPort = 4242;
+    m_netServer = std::make_unique<TestVeinServerWithNet>(serverPort);
+    m_netServer->getServer()->simulAllModulesLoaded(QString("foo"), QStringList() << "fooList");
+
+    m_netClient = std::make_unique<VfCoreStackClient>();
+    m_entityItem = VfEntityComponentEventItem::create(systemEntityId);
+    m_netClient->addItem(m_entityItem);
+    m_netClient->connectToServer("127.0.0.1", serverPort);
+    TimeMachineObject::feedEventLoop();
+}
+
+void test_client_command_events::cleanup()
+{
+    m_entityItem = nullptr;
+    m_netClient = nullptr;
+    m_netServer = nullptr;
+}
+
+void test_client_command_events::subscribeClient(int entityId)
+{
+    m_netClient->subscribeEntity(entityId);
+    TimeMachineObject::feedEventLoop();
 }
--- framework/tests/test_client_command_events.h
+++ framework/tests/test_client_command_events.h
@@ -2,6 +2,8 @@
 #define TEST_CLIENT_COMMAND_EVENTS_H
 
 #include "testveinserverwithnet.h"
+#include "vf_core_stack_client.h"
+#include "vf_entity_component_event_item.h"
 #include <memory>
 
 class test_client_command_events : public QObject
@@ -14,7 +16,10 @@ private slots:
     void subscribeSystemEntity();
     void fetchSystemEntityComponent();
 private:
+    void subscribeClient(int entityId);
     std::unique_ptr<TestVeinServerWithNet> m_netServer;
+    std::unique_ptr<VfCoreStackClient> m_netClient;
+    VfCmdEventItemEntityPtr m_entityItem;
 };
 
 #endif // TEST_CLIENT_COMMAND_EVENTS_H
