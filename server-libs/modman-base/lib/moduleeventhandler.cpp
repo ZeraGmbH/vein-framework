6687240b6b397fa639518adc97b4ab8512d89e8b
Andreas Müller <schnitzeltony@gmail.com>
2024-04-05 22:19:15 +0200

    Log the number of event systems with interesting results
    MT310s2 default session shows:
    Core event systems: 12
    Module event systems: 41 (!!!)
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modman-base/lib/moduleeventhandler.cpp
+++ server-libs/modman-base/lib/moduleeventhandler.cpp
@@ -1,13 +1,17 @@
 #include "moduleeventhandler.h"
 #include <QEvent>
 
-ModuleEventHandler::ModuleEventHandler(QObject *parent) : VeinEvent::EventHandler(parent)
+ModuleEventHandler::ModuleEventHandler(QObject *parent) :
+    VeinEvent::EventHandler(parent)
 {
+    connect(m_waitForAddSilenceToLogTimer.get(), &TimerTemplateQt::sigExpired,
+            this, &ModuleEventHandler::onAddSilence);
 }
 
 void ModuleEventHandler::addModuleSystem(VeinEvent::EventSystem *eventSystem)
 {
     if(m_moduleSystems.contains(eventSystem) == false) {
+        m_waitForAddSilenceToLogTimer->start();
         m_moduleSystems.append(eventSystem);
         eventSystem->attach(this);
     }
@@ -23,4 +27,9 @@ void ModuleEventHandler::customEvent(QEvent *event)
     for(int i=0; i < m_moduleSystems.count() && event->isAccepted()==false; ++i)
         m_moduleSystems.at(i)->processEvent(event);
     VeinEvent::EventHandler::customEvent(event);
+}
+
+void ModuleEventHandler::onAddSilence()
+{
+    qInfo("Module event systems: %i", m_moduleSystems.count());
 }
