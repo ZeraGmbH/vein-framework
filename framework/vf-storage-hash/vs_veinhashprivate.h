602be438e687595de3bb5daa4bdd9d1dcc75a1a4
Asmae Kaouissi <a.kaouissi@zera.de>
2024-06-28 09:56:11 +0200

    vs_veinhashprivate: rename time -> timestamp
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- framework/vf-storage-hash/vs_veinhashprivate.h
+++ framework/vf-storage-hash/vs_veinhashprivate.h
@@ -14,13 +14,13 @@ class StorageComponent : public VeinEvent::StorageComponentInterface
 public:
     StorageComponent(QVariant value);
     QVariant getValue() const override;
-    QString getTime() const override;
+    QString getTimestamp() const override;
 private:
     friend class VeinHashPrivate;
     void setValue(QVariant value);
-    void setTime(QString time);
+    void setTimestamp(QString timestamp);
     QVariant m_value;
-    QString m_time;
+    QString m_timestamp;
 };
 
 typedef std::shared_ptr<StorageComponent> StorageComponentPtr;
@@ -33,7 +33,7 @@ public:
     StorageComponentPtr getFutureComponent(int entityId, const QString &componentName);
     void insertComponentValue(EntityMap* entityChecked, const QString &componentName, QVariant value);
     void insertFutureComponent(int entityId, QString componentName, StorageComponentPtr component, QVariant value);
-    void changeComponentValue(StorageComponentPtr componentChecked, QVariant value, QString time);
+    void changeComponentValue(StorageComponentPtr componentChecked, QVariant value, QString timestamp);
     void removeComponentValue(EntityMap* entityChecked, const QString &componentName);
     void insertEntity(const int entityId);
     void removeEntity(const int entityId);
