b8c9cddd6494b33abd9798ebe861b192256474b4
Andreas Müller <schnitzeltony@gmail.com>
2024-10-10 22:19:00 +0200

    VfServerEntityRemove::generateEvent: Sync to implementations around
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- convenient-code/lib/command-event-actors/vf_server_entity_remove.cpp
+++ convenient-code/lib/command-event-actors/vf_server_entity_remove.cpp
@@ -6,8 +6,6 @@ QEvent *VfServerEntityRemove::generateEvent(int entityId)
 {
     VeinComponent::EntityData *eData = new VeinComponent::EntityData();
     eData->setEntityId(entityId);
-    eData->setEventOrigin(VeinEvent::EventData::EventOrigin::EO_LOCAL);
-    eData->setEventTarget(VeinEvent::EventData::EventTarget::ET_ALL);
     eData->setCommand(VeinComponent::EntityData::Command::ECMD_REMOVE);
     return new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::NOTIFICATION, eData);
 }
