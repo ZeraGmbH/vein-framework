da9e720da2c117fdb66c69e354482a50ff72134d
Andreas Müller <schnitzeltony@gmail.com>
2024-11-04 18:19:25 +0100

    vf-storage: Reduce AbstractDatabase to what is needed
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- framework/vf-storage/lib/vs_abstractdatabase.h
+++ framework/vf-storage/lib/vs_abstractdatabase.h
@@ -14,22 +14,15 @@ public:
     virtual bool hasEntity(int entityId) const = 0;
     virtual EntityMap *findEntity(const int entityId) = 0;
     virtual QList<int> getEntityList() const = 0;
-    virtual void insertEntity(const int entityId) = 0;
-    virtual void removeEntity(const int entityId) = 0;
 
     virtual bool hasStoredValue(int entityId, const QString &componentName) const = 0;
     virtual QVariant getStoredValue(int t_entityId, const QString &t_componentName) const = 0;
-    virtual StorageComponentPtr findComponent(EntityMap *entityMap, const QString &componentName) = 0;
     virtual StorageComponentPtr findComponent(const int entityId, const QString &componentName) = 0;
+    virtual StorageComponentPtr findComponent(EntityMap *entityMap, const QString &componentName) = 0;
     virtual QList<QString> getComponentList(int entityId) = 0;
-    virtual void insertComponentValue(EntityMap* entityChecked, const QString &componentName, QVariant value) = 0;
-    virtual void changeComponentValue(StorageComponentPtr componentChecked, QVariant value) = 0;
-    virtual void removeComponentValue(EntityMap* entityChecked, const QString &componentName) = 0;
 
     virtual bool areFutureComponentsEmpty() const = 0;
     virtual StorageComponentPtr getFutureComponent(int entityId, const QString &componentName) = 0;
-    virtual StorageComponentPtr takeFutureComponent(const int entityId, const QString &componentName) = 0;
-    virtual void insertFutureComponent(int entityId, QString componentName, StorageComponentPtr component, QVariant value) = 0;
 };
 
 }
--- framework/vf-storage/lib/vs_databasehash.h
+++ framework/vf-storage/lib/vs_databasehash.h
@@ -12,25 +12,32 @@ namespace VeinStorage
 class DatabaseHash : public AbstractDatabase
 {
 public:
+    // Outer interface
     bool hasEntity(int entityId) const override;
     EntityMap *findEntity(const int entityId) override;
     QList<int> getEntityList() const override;
-    void insertEntity(const int entityId) override;
-    void removeEntity(const int entityId) override;
 
     bool hasStoredValue(int entityId, const QString &componentName) const override;
     QVariant getStoredValue(int entityId, const QString &componentName) const override;
-    StorageComponentPtr findComponent(EntityMap *entityMap, const QString &componentName) override;
     StorageComponentPtr findComponent(const int entityId, const QString &componentName) override;
+    StorageComponentPtr findComponent(EntityMap *entityMap, const QString &componentName) override;
     QList<QString> getComponentList(int entityId) override;
-    void insertFutureComponent(int entityId, QString componentName, StorageComponentPtr component, QVariant value) override;
-    void changeComponentValue(StorageComponentPtr componentChecked, QVariant value) override;
-    void removeComponentValue(EntityMap* entityChecked, const QString &componentName) override;
 
     bool areFutureComponentsEmpty() const override;
     StorageComponentPtr getFutureComponent(int entityId, const QString &componentName) override;
-    void insertComponentValue(EntityMap* entityChecked, const QString &componentName, QVariant value) override;
-    StorageComponentPtr takeFutureComponent(const int entityId, const QString &componentName) override;
+
+    // Event system interface
+    // ATOW event systems use conrete DatabaseHash. Once we add another implementation
+    // of DatabaseHash, we have to think about an interface as AbstractDatabaseWritable
+    void insertEntity(const int entityId);
+    void removeEntity(const int entityId);
+
+    void insertComponentValue(EntityMap* entityChecked, const QString &componentName, QVariant value);
+    void changeComponentValue(StorageComponentPtr componentChecked, QVariant value);
+    void removeComponentValue(EntityMap* entityChecked, const QString &componentName);
+
+    void insertFutureComponent(int entityId, QString componentName, StorageComponentPtr component, QVariant value);
+    StorageComponentPtr takeFutureComponent(const int entityId, const QString &componentName);
 
 private:
     QHash<int, EntityMap> m_entityComponentData;
