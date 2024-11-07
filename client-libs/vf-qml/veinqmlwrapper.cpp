a31846198f11eacdde09cf054474ae9b406fec18
Andreas Müller <schnitzeltony@gmail.com>
2024-11-07 22:47:09 +0100

    veinqmlwrapper: Fix indentation
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- client-libs/vf-qml/veinqmlwrapper.cpp
+++ client-libs/vf-qml/veinqmlwrapper.cpp
@@ -9,28 +9,26 @@
 namespace VeinApiQml
 {
 
-  QmlWrapper::QmlWrapper()
-  {
+QmlWrapper::QmlWrapper()
+{
+}
 
-  }
-
-  QObject *QmlWrapper::getSingletonInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine)
-  {
+QObject *QmlWrapper::getSingletonInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine)
+{
     Q_UNUSED(t_engine);
     Q_UNUSED(t_scriptEngine);
-
     return VeinApiQml::VeinQml::getStaticInstance();
-  }
+}
 
-  void registerTypes()
-  {
+void registerTypes()
+{
     // @uri Vein
     using namespace VeinApiQml;
     qmlRegisterSingletonType<VeinQml>("VeinEntity", 1, 0, "VeinEntity", QmlWrapper::getSingletonInstance);
     qmlRegisterInterface<EntityComponentMap>("EntityComponentMap");//, 1, 0, "VeinEntityMap", QString("VeinEntityMap is not creatable"));
-  }
+}
 
-  Q_COREAPP_STARTUP_FUNCTION(registerTypes)
+Q_COREAPP_STARTUP_FUNCTION(registerTypes)
 
 
 } // namespace VeinApiQml
