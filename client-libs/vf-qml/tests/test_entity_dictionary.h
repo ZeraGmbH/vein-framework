997045e8d63cda8116eb742b7765e13e0654c37e
Andreas Müller <schnitzeltony@gmail.com>
2024-11-12 22:58:30 +0100

    Introduce EntityDictionary to fasten entity search by names
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ client-libs/vf-qml/tests/test_entity_dictionary.h
@@ -0,0 +1,29 @@
+#ifndef TEST_ENTITY_DICTIONARY_H
+#define TEST_ENTITY_DICTIONARY_H
+
+#include <QObject>
+
+class test_entity_dictionary : public QObject
+{
+    Q_OBJECT
+private slots:
+    void findsOnEmpty();
+    void insertOneAndFindById();
+    void insertTwoSameId();
+    void insertTwoAndFind();
+    void insertOneAndDelete();
+
+    void insertOneSetName();
+    void insertOneSetNameInvalidId();
+    void insertOneSetNameFind();
+    void insertOneSetTwoNames();
+    void insertTwoSetTwoNames();
+    void insertTwoSetTwoNamesNotUnique();
+    void insertTwoSetFourNames();
+    void insertOneRemoveFindName();
+
+    void entryList();
+    void nameFromId();
+};
+
+#endif // TEST_ENTITY_DICTIONARY_H
