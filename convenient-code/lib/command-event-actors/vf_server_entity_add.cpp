ae32284003c905a8d7f3b8384b5fa0fb622919f7
Andreas Müller <schnitzeltony@gmail.com>
2024-11-14 19:09:24 +0100

    vs_clientstorageeventsystem: Set target to local explicitly and explain why
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- convenient-code/lib/command-event-actors/vf_server_entity_add.cpp
+++ convenient-code/lib/command-event-actors/vf_server_entity_add.cpp
@@ -6,6 +6,9 @@ QEvent *VfServerEntityAdd::generateEvent(int entityId)
 {
     VeinComponent::EntityData *eData = new VeinComponent::EntityData();
     eData->setCommand(VeinComponent::EntityData::Command::ECMD_ADD);
+    // Only subscribed are notified + subscription on non existant is not possible
+    // Don't transfer over network
+    eData->setEventTarget(VeinEvent::EventData::EventTarget::ET_LOCAL);
     eData->setEntityId(entityId);
     return new VeinEvent::CommandEvent(VeinEvent::CommandEvent::EventSubtype::NOTIFICATION, eData);
 }
