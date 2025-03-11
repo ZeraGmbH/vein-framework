69b9f8c05022f36ad59f1d70b6deaf97765f2c01
Asmae Kaouissi <a.kaouissi@zera.de>
2025-03-11 16:36:42 +0100

    add test_task_client_rpc_invoker
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ convenient-code/tests/test_task_client_rpc_invoker.h
@@ -0,0 +1,15 @@
+#ifndef TEST_TASK_CLIENT_RPC_INVOKER_H
+#define TEST_TASK_CLIENT_RPC_INVOKER_H
+
+#include <QObject>
+
+class test_task_client_rpc_invoker : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void invokeSubscribedRPC();
+    void timeout();
+};
+
+#endif // TEST_TASK_CLIENT_RPC_INVOKER_H
