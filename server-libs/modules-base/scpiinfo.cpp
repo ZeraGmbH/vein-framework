2d97c954fd35e442a7f691e7e702f4bd8d12c42e
Andreas Müller <schnitzeltony@gmail.com>
2025-04-06 01:45:41 +0200

    scpiinfo: Rename variables for readability
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- server-libs/modules-base/scpiinfo.cpp
+++ server-libs/modules-base/scpiinfo.cpp
@@ -1,15 +1,20 @@
 #include "scpiinfo.h"
 
-cSCPIInfo::cSCPIInfo(QString model, QString cmd, QString cmdtype, QString refname, QString reftype, QString unit) :
-    m_sSCPIModel(model), m_sSCPICmd(cmd), m_sSCPICmdType(cmdtype), m_sRefName(refname), m_sRefType(reftype), m_sUnit(unit)
+cSCPIInfo::cSCPIInfo(QString model, QString cmd, QString cmdtype, QString veinComponentName, QString reftype, QString unit) :
+    m_sSCPIModel(model), m_sSCPICmd(cmd), m_sSCPICmdType(cmdtype), m_veinComponentName(veinComponentName), m_sRefType(reftype), m_sUnit(unit)
 {
 }
 
-cSCPIInfo::cSCPIInfo(QString model, QString cmd, int cmdTypeMask, QString refname, SCPI::eSCPIEntryType entryType, QString unit) :
+cSCPIInfo::cSCPIInfo(QString model,
+                     QString cmd,
+                     int cmdTypeMask,
+                     QString veinComponentName,
+                     SCPI::eSCPIEntryType entryType,
+                     QString unit) :
     m_sSCPIModel(model),
     m_sSCPICmd(cmd),
     m_sSCPICmdType(QString::number(cmdTypeMask)),
-    m_sRefName(refname),
+    m_veinComponentName(veinComponentName),
     m_sRefType(entryType==SCPI::isCatalog ? "1" : "0"),
     m_sUnit(unit)
 {
@@ -21,7 +26,7 @@ void cSCPIInfo::appendSCPIInfo(QJsonArray &jsArr)
     jsonSCPIArr.append(m_sSCPIModel);
     jsonSCPIArr.append(m_sSCPICmd);
     jsonSCPIArr.append(m_sSCPICmdType);
-    jsonSCPIArr.append(m_sRefName);
+    jsonSCPIArr.append(m_veinComponentName);
     jsonSCPIArr.append(m_sRefType);
     jsonSCPIArr.append(m_sUnit);
 
--- server-libs/modules-base/scpiinfo.h
+++ server-libs/modules-base/scpiinfo.h
@@ -16,8 +16,8 @@ class cSCPIInfo
 public:
 
     cSCPIInfo(){}
-    cSCPIInfo(QString model, QString cmd, QString cmdtype, QString refname, QString reftype, QString unit);
-    cSCPIInfo(QString model, QString cmd, int cmdTypeMask, QString refname, SCPI::eSCPIEntryType entryType, QString unit);
+    cSCPIInfo(QString model, QString cmd, QString cmdtype, QString veinComponentName, QString reftype, QString unit);
+    cSCPIInfo(QString model, QString cmd, int cmdTypeMask, QString veinComponentName, SCPI::eSCPIEntryType entryType, QString unit);
 
     void appendSCPIInfo(QJsonArray &jsArr);
 
@@ -25,7 +25,7 @@ private:
     const QString m_sSCPIModel;
     const QString m_sSCPICmd;
     const QString m_sSCPICmdType;
-    const QString m_sRefName;
+    const QString m_veinComponentName;
     const QString m_sRefType;
     const QString m_sUnit;
 };
