89b46256638e557c31e94414765f8ba63cc3db62
amutalik <a.mutalik@zera.de>
2025-05-08 11:29:17 +0200

    vf-cpp/testlib: Introduce VfEntityWithRpcSimplified
    To be used for testing new rpc implementation. It is stripped down version of VfCppEntity. It should be removed when VfCppEntity starts using new rpc implementation.
    
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- server-libs/vf-cpp/lib/vf-cpp-rpc-simplified.h
+++ server-libs/vf-cpp/lib/vf-cpp-rpc-simplified.h
@@ -8,11 +8,14 @@ class VfCppRpcSimplified
 {
 public:
     VfCppRpcSimplified(VeinEvent::EventSystem *eventSystem, int entityId, QString rpcName, QMap<QString,QString> parameters);
+    QString getSignature();
+    void callFunction(const QUuid &callId, const QUuid &peerId, const QVariantMap &parameters);
 private:
     QString createRpcSignature(QString rpcName, QMap<QString,QString> paramDescriptions);
     VeinEvent::EventSystem *m_eventSystem;
     int m_entityId;
     QString m_rpcSignature;
 };
+typedef std::shared_ptr<VfCppRpcSimplified> VfCppRpcSimplifiedPtr;
 
 #endif // VFCPPRPCSIMPLIFIED_H
---
+++ server-libs/vf-cpp/testlib/vfentitywithrpcsimplified.h
@@ -0,0 +1,22 @@
+#ifndef VFENTITYWITHRPCSIMPLIFIED_H
+#define VFENTITYWITHRPCSIMPLIFIED_H
+
+#include "ve_commandevent.h"
+#include "ve_eventsystem.h"
+#include "vf-cpp-rpc-simplified.h"
+
+class VfTEntityWithRpcSimplified : public VeinEvent::EventSystem
+{
+public:
+    VfTEntityWithRpcSimplified(int entityId);
+    ~VfTEntityWithRpcSimplified();
+    void initModule();
+    void createRpc(QString rpcName, QMap<QString,QString> parameters);
+private:
+    void processEvent(QEvent *event) override;
+    void handleRpcs(VeinEvent::CommandEvent *cmdEvent);
+    int m_entityId;
+    QMap<QString,VfCppRpcSimplifiedPtr> m_rpcList;
+};
+
+#endif // VFENTITYWITHRPCSIMPLIFIED_H
