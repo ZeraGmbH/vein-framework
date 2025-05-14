c97ed7838507ff50a84d1ae55213e8063cc2f38a
amutalik <a.mutalik@zera.de>
2025-05-14 11:01:37 +0200

    test_rpc_simplified: More tests
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- server-libs/vf-cpp/testlib/vftestrpcsimplified.h
+++ server-libs/vf-cpp/testlib/vftestrpcsimplified.h
@@ -2,6 +2,7 @@
 #define VFTESTRPCSIMPLIFIED_H
 
 #include "vfentitywithrpcsimplified.h"
+#include <timerfactoryqt.h>
 
 class VfTestRpcSimplified : public QObject
 {
@@ -13,10 +14,12 @@ public:
     void RPC_PublicMethod(QVariantMap parameters);
 public slots:
     void RPC_forTest(QVariantMap parameters);
+    void RPC_addDelay(QVariantMap parameters);
 private:
     void RPC_PrivateMethod(QVariantMap parameters);
     std::shared_ptr<VfEntityWithRpcSimplified> m_entity;
     bool m_initialized = false;
+    TimerTemplateQtPtr m_delayResponse;
 };
 
 #endif // VFTESTRPCSIMPLIFIED_H
