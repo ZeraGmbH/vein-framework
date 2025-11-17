ad8185d1797a80205235ab117edb03101de1907d
amutalik <a.mutalik@zera.de>
2025-11-17 09:34:21 +0100

    vf-cpp-rpc-helper: Add more helper functions
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- server-libs/vf-cpp/lib/vf-cpp-rpc-helper.cpp
+++ server-libs/vf-cpp/lib/vf-cpp-rpc-helper.cpp
@@ -22,3 +22,29 @@ QStringList VfCppRpcHelper::getRpcTypesListFromSignature(const QString &rpcSigna
     QString typesCommaSeparated = getRpcTypesFromSignature(rpcSignature);
     return typesCommaSeparated.split(",", Qt::SkipEmptyParts);
 }
+
+QStringList VfCppRpcHelper::getRpcParamNamesList(QString RPC)
+{
+    // RPC_name(type p_paramName1, type p_paramName2)
+    QStringList parameterNames;
+    QRegularExpression re("\\((.*)\\)");
+    QRegularExpressionMatch match = re.match(RPC);
+
+    if (match.hasMatch()) {
+        QString params = match.captured(1).trimmed();
+        const QStringList paramsList = params.split(",", Qt::SkipEmptyParts);
+
+        for (const QString &parameter : paramsList) {
+            QStringList paramTypeValue = parameter.split(" ");
+            if (paramTypeValue.size() == 2)
+                parameterNames.append(paramTypeValue[1]);
+        }
+    }
+    return parameterNames;
+}
+
+QString VfCppRpcHelper::getRpcName(QString RPC)
+{
+    QString RPCName = RPC.section('(', 0, 0);
+    return RPCName;
+}
--- server-libs/vf-cpp/lib/vf-cpp-rpc-helper.h
+++ server-libs/vf-cpp/lib/vf-cpp-rpc-helper.h
@@ -8,6 +8,8 @@ class VfCppRpcHelper
 public:
     static QString getRpcTypesFromSignature(const QString &rpcSignature);
     static QStringList getRpcTypesListFromSignature(const QString &rpcSignature);
+    static QStringList getRpcParamNamesList(QString RPC);
+    static QString getRpcName(QString RPC);
 };
 
 #endif // VF_CPP_RPC_HELPER_H
