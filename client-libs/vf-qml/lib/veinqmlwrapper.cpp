6dc3d1fa9cf2f27eabdbcb5fa36e1143dca6d642
Andreas Müller <schnitzeltony@gmail.com>
2025-12-20 22:41:08 +0100

    Fix Qt5.15 warning on qmlRegisterInterface - Qt6 will fail
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- client-libs/vf-qml/lib/veinqmlwrapper.cpp
+++ client-libs/vf-qml/lib/veinqmlwrapper.cpp
@@ -25,7 +25,11 @@ void registerTypes()
     // @uri Vein
     using namespace VeinApiQml;
     qmlRegisterSingletonType<VeinQml>("VeinEntity", 1, 0, "VeinEntity", QmlWrapper::getSingletonInstance);
-    qmlRegisterInterface<EntityComponentMap>("EntityComponentMap");//, 1, 0, "VeinEntityMap", QString("VeinEntityMap is not creatable"));
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
+    qmlRegisterInterface<EntityComponentMap>("EntityComponentMap");
+#else
+    qmlRegisterInterface<EntityComponentMap>("EntityComponentMap", 1);
+#endif
 }
 
 Q_COREAPP_STARTUP_FUNCTION(registerTypes)
